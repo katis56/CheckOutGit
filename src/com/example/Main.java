6723243cc92d1a1853f25789dce3ea44e6f97b5c
Kat Czyz <czyzkat@gmail.com>
2019-01-19 15:21:01 +0100

    Added loop

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("cosie");
+
+        for (int i = 1; i <11 ; i++) {
+            System.out.println(i);
+        }
     }
 }
